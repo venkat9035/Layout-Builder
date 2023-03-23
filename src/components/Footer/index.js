022829d3b1d7a1aab4a73620c9f5cf2f71827f66
venkat9035 <m.venky1309@gmail.com>
2023-03-23 05:27:51 +0000

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+// Write your code here
+
+import './index.css'
+
+const Footer = () => (
+  <div className="footer-container">
+    <h1 className="footer-text">Footer</h1>
+  </div>
+)
+
+export default Footer
